41b7862f093f8430b8d35400a70d55e6064a9a66
onielrosario <onielrosario@pursuit.org>
2018-11-14 14:23:56 -0500

    first commit

---
+++ AutoLayoutPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutPractice
+//
+//  Created by Oniel Rosario on 11/14/18.
+//  Copyright © 2018 Oniel Rosario. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ print("oka")
+    }
+
+
+}
+
